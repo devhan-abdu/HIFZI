9e40bacf07777d310ec0c31934d1c07758cdd2e7
devhan-abdu <hanabduman@gmail.com>
2026-05-15 09:26:47 +0300

    chore(config): tidy app config and refresh splash logo asset

--- app.config.js
+++ app.config.js
@@ -8,21 +8,18 @@ export default ({ config }) => {
       appName: "Hifzi (Dev)",
       scheme: "hifzi-dev",
     },
-
     preview: {
       androidPackage: "com.devhananabdu.hifzi.preview",
       iosBundle: "com.devhananabdu.hifzi.preview",
       appName: "Hifzi (Preview)",
       scheme: "hifzi-preview",
     },
-
     production: {
       androidPackage: "com.devhananabdu.hifzi",
       iosBundle: "com.devhananabdu.hifzi",
       appName: "Hifzi",
       scheme: "hifzi",
     },
-
     "production-apk": {
       androidPackage: "com.devhananabdu.hifzi",
       iosBundle: "com.devhananabdu.hifzi",
@@ -35,54 +32,43 @@ export default ({ config }) => {
 
   return {
     ...config,
-
     name: active.appName,
     slug: "hifzi",
     version: "1.0.0",
-
+    runtimeVersion: "1.0.0",
     scheme: active.scheme,
-
     orientation: "portrait",
     userInterfaceStyle: "automatic",
     newArchEnabled: true,
-
     icon: "./assets/images/minilogo.png",
-
-    splash: {
-      image: "./assets/images/hifzilogowhite.png",
-      resizeMode: "contain",
-      backgroundColor: "#276359",
-    },
-
     ios: {
       bundleIdentifier: active.iosBundle,
       supportsTablet: true,
+      buildNumber: "1",
     },
-
     android: {
       package: active.androidPackage,
-
+      versionCode: 1,
       adaptiveIcon: {
         foregroundImage: "./assets/images/minilogo.png",
         backgroundColor: "#E6F4FE",
         monochromeImage: "./assets/images/minilogo.png",
       },
-
-      edgeToEdgeEnabled: true,
-      predictiveBackGestureEnabled: false,
+      permissions: [
+        "android.permission.INTERNET",
+        "android.permission.READ_EXTERNAL_STORAGE",
+        "android.permission.WRITE_EXTERNAL_STORAGE",
+      ],
     },
-
     web: {
       bundler: "metro",
       favicon: "./assets/images/favicon.png",
     },
-
     extra: {
       eas: {
         projectId: "b30fb5cd-a8e2-4463-bbea-5df0ca4348bb",
       },
     },
-
     plugins: [
       [
         "expo-splash-screen",
@@ -91,17 +77,14 @@ export default ({ config }) => {
           imageWidth: 200,
           resizeMode: "contain",
           backgroundColor: "#276359",
-
           dark: {
             backgroundColor: "#1e293b",
           },
         },
       ],
-
       "expo-sqlite",
       "expo-audio",
       "expo-asset",
-
       [
         "expo-font",
         {
